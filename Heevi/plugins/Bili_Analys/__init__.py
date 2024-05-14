dd1862e09b266dd41fa0dd4b9695783fb8155ea3
TsukasaYahane <TsukasaYahane@outlook.com>
2024-05-14 15:11:10 +0800

    :hammer:重构了解析算法

--- Heevi/plugins/Bili_Analys/__init__.py
+++ Heevi/plugins/Bili_Analys/__init__.py
@@ -1,17 +1,38 @@
 from nonebot import on_message
-from nonebot.rule import keyword
+from nonebot.rule import regex
 from nonebot.exception import FinishedException
-from nonebot.params import EventPlainText
-from nonebot.adapters.onebot.v11 import MessageSegment
-from .get_info import get_videoinfo
+from nonebot.adapters.onebot.v11 import MessageSegment,Event
+from .get_info import get_videoinfo,b23tv_get
+import re
+import json
 
-Bili_Analys = on_message(rule=keyword('bilibili.com/video/'),priority=10,block=True)
+pattern = r'(b23.tv/|bilibili.com/video/|^BV[a-zA-Z0-9]{10})'
 
-@Bili_Analys.handle()
-async def Video_Analys(message=EventPlainText()):
-    try :
-        Cut = message.index('BV')
-        BV = message[Cut+2:Cut+12]
+Bili_Analys = on_message(rule = regex(pattern,flags=re.I),priority=10,block=True)
+
+@Bili_Analys.handle()    
+async def Video_Analys(event:Event): 
+    try :    
+        content= str(event.get_message())
+        try :
+            data = json.loads(content)
+            if data.get('title')=='哔哩哔哩' or '[QQ小程序]' in data.get('prompt') :
+                qqdocurl = data['meta']['detail_1']['qqdocurl']
+                Cut = qqdocurl.index('BV')
+                BV = qqdocurl[Cut+2:Cut+12]
+        except json.JSONDecodeError :
+            url_pattern = re.compile(r'https?://(?:b23\.tv/\S+|)', re.IGNORECASE)
+            urls = url_pattern.findall(content)
+            if urls :
+                url = urls[0]
+                try :
+                    if 'b23.tv' in url :
+                        BV = b23tv_get(url)
+                except Exception as e :
+                    await Bili_Analys.finish(f"视频解析错误：{e}")
+            else :
+                Cut = content.index('BV')
+                BV = content[Cut+2:Cut+12]
         data = get_videoinfo(BV)
         info = data['data']
         Video_Name = info['title']
@@ -27,7 +48,12 @@ async def Video_Analys(message=EventPlainText()):
         Video_Coin =video_data['coin']
         Video_Share = video_data['share']
         Video_pic = info['pic']
-        await Bili_Analys.finish(MessageSegment.image(Video_pic)+MessageSegment.text("视频标题：{}\t视频分区：{}\nup主：{}\t播放量：{}\n点赞：{}\t投币：{}\n收藏：{}\t评论：{}\n弹幕：{}\t分享：{}\n视频简介：{}".format(Video_Name,Video_Area,Video_Oner,Video_view,Video_Like,Video_Coin,Video_Favorite,Video_Reply,Video_Danmu,Video_Share,Video_desc)))
+        await Bili_Analys.finish(MessageSegment.image(Video_pic)
+                                 +MessageSegment.text("【视频标题】：{}\n"+"【视频分区||up主】：{}||{}\n"
+                                 +"【播放量】：{}\t【点赞】：{}\t【投币】：{}\t【收藏】：{}\n"
+                                 +"【评论】：{}\t【弹幕】：{}\t【分享】：{}\n【视频简介】：{}"
+                                 .format(Video_Name,Video_Area,Video_Oner,Video_view,Video_Like,
+                                  Video_Coin,Video_Favorite,Video_Reply,Video_Danmu,Video_Share,Video_desc)))
     except FinishedException :
         raise
     except Exception as e :
--- Heevi/plugins/Bili_Analys/get_info.py
+++ Heevi/plugins/Bili_Analys/get_info.py
@@ -1,15 +1,21 @@
 import requests
-from io import BytesIO
-import base64
 
-def get_videoinfo(BV) :
-    url = 'https://api.bilibili.com/x/web-interface/view'
-    headers = {
+headers = {
     'Acept-Language' : 'zh-CN,zh;q=0.7',
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
     }
+
+def get_videoinfo(BV) :
+    url = 'https://api.bilibili.com/x/web-interface/view'
     data = requests.get(url,params={
     'bvid' : BV
     },headers=headers,timeout = 10)
     video_data = data.json()
     return video_data
+
+def b23tv_get(url:str):
+    response = requests.get(url,headers=headers,timeout = 10)
+    final_url = response.url
+    Cut = final_url.index('BV')
+    BV = final_url[Cut+2:Cut+12]
+    return BV
